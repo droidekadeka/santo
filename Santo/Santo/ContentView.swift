d8a0daddd59b096ea18447cee48666c50e2b8c2a
Anastasiia <anastasiia@Anastasiias-MacBook-Air.local>
2024-06-03 22:06:31 +0500

    swiftUI 1lesson

--- Santo/Santo/ContentView.swift
+++ Santo/Santo/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("hello strong one")
+                .fontWeight(.light)
+            Text("keep going")
+                .font(.largeTitle)
+                .fontWeight(.light)
+                .foregroundColor(.green)
         }
         .padding()
     }
---
+++ Santo/Santo/mytime.swift
@@ -0,0 +1,18 @@
+//
+//  mytime.swift
+//  Santo
+//
+//  Created by Anastasiia on 03.06.2024.
+//
+
+import SwiftUI
+
+struct mytime: View {
+    var body: some View {
+        Text("My Time")
+    }
+}
+
+#Preview {
+    mytime()
+}
