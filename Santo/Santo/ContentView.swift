676c0b3f7ca1f30377d679b07bb1902b931248bf
Anastasiia <anastasiia@Anastasiias-MacBook-Air.local>
2024-06-03 14:51:43 +0500

    Initial commit

---
+++ Santo/Santo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Santo
+//
+//  Created by Anastasiia on 03.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hi")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Santo/Santo/SantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SantoApp.swift
+//  Santo
+//
+//  Created by Anastasiia on 03.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
